4773102377a6eff08145ca7d804f86378d7f5826
IlhamAbaslı <ilhamra@code.edu.az>
2024-03-08 22:36:30 +0400

    counter added, project finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,16 @@
 "use strict";
-
+//#region slider
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
-  spaceBetween: 30,
+  spaceBetween: 0,
   loop: true,
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
 });
+
+//#endregion
 
 
 //#region search
@@ -29,6 +23,11 @@ const searchClose = document.querySelector(".search-area .search-input i");
 searchBtn.addEventListener("click",function(){
   searchArea.classList.add("active");
   searchInput.classList.remove("passive");
+})
+
+searchInput.addEventListener("click",function(){
+  searchArea.classList.remove("active");
+  searchInput.classList.add("passive");
 })
 
 
@@ -66,12 +65,58 @@ closeDropBtn.addEventListener("click",function(){
 //#endregion
 
 
-
+//#region accordion
 var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
 var collapseList = collapseElementList.map(function (collapseEl) {
   return new bootstrap.Collapse(collapseEl)
 })
 
+//#endregion
+
+
+//#region counter
+
+let count1 = document.querySelector("#report .datas .col-12:nth-child(1) h2");
+let count2 = document.querySelector("#report .datas .col-12:nth-child(2) h2");
+let count3 = document.querySelector("#report .datas .col-12:nth-child(3) h2");
+let count4 = document.querySelector("#report .datas .col-12:nth-child(4) h2");
+
+function animateValue(item, start, end, duration) {
+    if (start === end) return;
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = item
+    var timer = setInterval(function() {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+function animateValue1(item, start, end, duration) {
+  if (start === end) return;
+  var range = end - start;
+  var current = start;
+  var increment = end > start? 20 : -1;
+  var stepTime = Math.abs(Math.floor(duration / range));
+  var obj = item
+  var timer = setInterval(function() {
+      current += increment;
+      obj.innerHTML = current;
+      if (current == end) {
+          clearInterval(timer);
+      }
+  }, stepTime);
+}
+
+animateValue1(count1, 0, 3000, 2000);
+animateValue(count2, 0, 427, 1000);
+animateValue(count3, 0, 95, 1000);
+animateValue(count4, 0, 43, 1000);
 
 
 
@@ -79,9 +124,7 @@ var collapseList = collapseElementList.map(function (collapseEl) {
 
 
 
-
-
-
+//#endregion
 
 
 
